8ed569c56e869e0641795ae7d1b17b9aa7b59c1f
Jack Davidson <jack.davidson@cox.net>
2017-10-07 15:43:20 -0700

    removed hardcoded thrift version

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -4,7 +4,8 @@ thrift --gen java isalive.thrift
 mv gen-java/* ./code
 
 cd ./code
-mvn package
+# build, using the thrift version you have installed.
+thrift --version | awk '{print $3}' | xargs bash -c 'mvn package -Dthrift.version=$0'
 
 cd target
 java -jar some-artifact-1.0.0-SNAPSHOT-jar-with-dependencies.jar
